cf5e5f9107d8b5d78a7fd8b1c7d99eb977a3b7b9
David Edwards <david.edwards@gmail.com>
2021-08-21 15:18:38 -0600

    Initiate first question

---
+++ driver.py
@@ -0,0 +1,17 @@
+import sys
+from pyke import knowledge_engine, krb_traceback
+
+engine = knowledge_engine.engine(__file__)
+
+1
+def run():
+    engine.reset()
+    try:
+        engine.activate('pattern_matching')
+        engine.prove_1_goal('pattern_matching.knows_genre()')
+    except Exception:
+        krb_traceback.print_exc()
+        sys.exit(1)
+
+
+run()
